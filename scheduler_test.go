0beea38488ceb559c4f05dc9d71e1e102d232b15
bigdrum <bigdrum0@gmail.com>
2016-07-23 10:23:03 -0400

    A little bit more test.

--- scheduler_test.go
+++ scheduler_test.go
@@ -9,11 +9,16 @@ import (
 func TestScheduler(t *testing.T) {
 	dataloader.RunWithScheduler(func(sch *dataloader.Scheduler) {
 		n := dataloader.NewNotification(sch)
+		k := 0
+		expectedK := 0
 		waitForK := func() {
+			k++
 			n.Wait()
 		}
 		sch.SpawnLow(func() {
 			t.Log("Run idle")
+			k += 100
+			expectedK = 102
 			n.Notify()
 			sch.Spawn(func() {
 				t.Log("6")
@@ -25,11 +30,17 @@ func TestScheduler(t *testing.T) {
 				t.Log("3")
 			})
 			waitForK()
+			if k != expectedK {
+				t.Error(k, expectedK)
+			}
 			t.Log("4")
 		})
 		sch.Spawn(func() {
 			t.Log("2")
 			waitForK()
+			if k != expectedK {
+				t.Error(k, expectedK)
+			}
 			t.Log("5")
 		})
 	})
